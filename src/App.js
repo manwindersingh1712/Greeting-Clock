8c71ee0758dddfb163a71798d5b2857bf2c5ccd5
Manwinder Singh <manwindersingh1712@gmail.com>
2020-10-01 23:03:15 +0530

    Digital clock with greeting

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
+var message="";
+
+var styles={
+  color:''
+};
+
+
+let curdate = new Date();
+let curHour=curdate.getHours();
+
+if(curHour<12){
+  message="Good Morning";
+  styles.color="green";
+}else if(curHour>=12 && curHour<19){
+  message="Good Afternoon";
+  styles.color="Orange";
+}else{
+  message="Good Night";
+  styles.color="black";
+}
+
 
 function App() {
+  let time=curdate.toLocaleTimeString();
+  
+  const [ctime,setCtime]= useState(time);
+
+  const updateTime=()=>{
+    time=new Date().toLocaleTimeString();
+    setCtime(time);
+  }
+
+
+  setInterval(updateTime,1000);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="center">
+
+      <div className>
+        <h1>
+        Hello There!! <span style={styles}>{message}</span>
+        </h1>
+        
+        <div id="time">
+          <h2>Time : {ctime}</h2>
+        </div>
+        </div>
     </div>
   );
 }
+
+
+
 
 export default App;
